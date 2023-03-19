5b8741da360d97bc0dcc2f9e2528af57e4bbda1b
odyyyy <iliya.odyvan4ikys@gmail.com>
2023-03-19 22:03:15 +0300

    :)

--- gui.py
+++ gui.py
@@ -1,26 +1,34 @@
 import tkinter as tk
-from tkinter import ttk
 from PIL import Image, ImageDraw
+import test
+import main
 
 
 # Получаем координаты курсора мыши и реализуем рисование
 def paint(event):
     x1, y1 = (event.x - 1), (event.y - 1)
     x2, y2 = (event.x + 1), (event.y + 1)
-    canvas.create_oval(x1, y1, x2, y2, fill="black", width=8)
-    draw.line([x1, y1, x2, y2], fill='black', width=8)
+    canvas.create_oval(x1, y1, x2, y2, fill="black", width=20)
+    draw.line([x1, y1, x2, y2], fill='black', width=25)
 
 
 # Сохранение нарисованного в png
 def save_image():
-    FILENAME = 'image.png'
-    out = img.resize(RESIZE)  # Преобразуем изображение в 28x28 пикселей
-    out.save(FILENAME)
+    FILENAME = 'image.jpg'
+    img.save(FILENAME)
+    # text_result = tk.Label(root, text=f'{main.recognize()[0]}', font=("Arial Black", 120), bg="#04819E", fg='#0f3753')
+    # text_result.place(x=CANVAS_WIDTH * 2 + 25, y=CANVAS_HEIGHT // 2)
 
+    text_result = tk.Label(root, text=f'{test.recognize()[0]}', font=("Arial Black", 120), bg="#04819E", fg='#0f3753')
+    text_result.place(x=CANVAS_WIDTH * 2 + 25, y=CANVAS_HEIGHT // 2)
+
+    text_percent = tk.Label(root, text=f'{test.recognize()[1]}', font=("Arial", 30), bg="#04819E", fg='#0f3753')
+    text_percent.place(x=CANVAS_WIDTH * 2 + 46, y=HEIGHT - 200)
 
 # Очистка холста
 def clear():
     canvas.delete(tk.ALL)
+    draw.rectangle((0, 0, img.size[0], img.size[1]), fill=(255, 255, 255))
 
 
 # Создаем окно
@@ -32,19 +40,20 @@ HEIGHT = 600
 
 root.geometry(f"{WIDTH}x{HEIGHT}+0+0")  # установка размера окна приложения с координатами (0,0) в левом верхнем углу
 root.resizable(False, False)
-root.title("Number recognizer")
-frame_left = tk.Frame(root, bg="#1d5d80", width=WIDTH // 2 - 10, height=HEIGHT - 100)
-frame_right = tk.Frame(root, bg="#1d5d80", width=WIDTH // 2 - 10, height=HEIGHT - 100)
-frame_top = tk.Frame(root, bg="#358abf", width=WIDTH, height=70)
+root.title("Digit recognizer")
+frame_left = tk.Frame(root, bg="#04819E", width=WIDTH // 2 - 10, height=HEIGHT - 100)
+frame_right = tk.Frame(root, bg="#04819E", width=WIDTH // 2 - 10, height=HEIGHT - 100)
+frame_top = tk.Frame(root, bg="#38B2CE", width=WIDTH, height=70)
 
 frame_left.pack(side='left', padx=0)
 frame_right.pack(side='right', padx=0)
 frame_top.place(x=0, y=0)
 
 # Установка заднего фона
-root.config(bg="#114a69")  # Установка цвета заднего фона
+root.config(bg="#03677F")  # Установка цвета заднего фона
 
-CANVAS_WIDTH = 300  # Ширина полотна
+# Размер холста и другие константы
+CANVAS_WIDTH = 300
 CANVAS_HEIGHT = 300
 WHITE = (255, 255, 255)
 RESIZE = (28, 28)
@@ -65,18 +74,21 @@ img = Image.new('RGB', (CANVAS_WIDTH, CANVAS_HEIGHT), WHITE)
 draw = ImageDraw.Draw(img)
 
 # Создаем, отображаем и настраиваем положение кнопок
-clear_button = tk.Button(root, text="Очистить", command=clear, bg="#358abf",
+clear_button = tk.Button(root, text="Очистить", command=clear, bg="#38B2CE",
                          fg="black", font=("Helvetica", 16), relief='flat')
-guess_button = tk.Button(root, text="Распознать", command=save_image, bg="#358abf",
+guess_button = tk.Button(root, text="Распознать", command=save_image, bg="#38B2CE",
                          fg="black", font=("Helvetica", 16), relief='flat')
 
 clear_button.place(x=CANVAS_WIDTH // 2 + 15, y=HEIGHT - 120)
 guess_button.place(x=CANVAS_WIDTH * 2 + 10, y=HEIGHT - 120)
 
-text = ttk.Label(text='Нарисуйте цифру', font=("Helvetica", 16))
-# text.place(x=CANVAS_WIDTH // 2 - 40, y=CANVAS_HEIGHT // 2 - 50)
-text_title = tk.Label(root, text="NUMBER RECOGNIZER", font=("Impact", 30), bg="#358abf", fg='#0f3753')
+# Добавление текста
+text_title = tk.Label(root, text="DIGIT RECOGNIZER", font=("Impact", 30), bg="#38B2CE", fg='#0f3753')
 text_title.place(x=HEIGHT // 2, y=5)
+
+text_res = tk.Label(root, text='Результат:', font=("Arial Black", 20), bg="#38B2CE", fg='#0f3753')
+text_res.place(x=CANVAS_WIDTH * 2 - 5, y=CANVAS_HEIGHT // 2 - 50)
+
 
 # Запускаем главный цикл обработки событий
 root.mainloop()
--- test.py
+++ test.py
@@ -1,129 +1,79 @@
-# import tkinter as tk
-#
-#
-# class DrawingApp:
-#     def __init__(self, master):
-#         master = master
-#         master.title("Drawing App")
-#         master.geometry("900x600")
-#
-#         # Добавляем изображение фона
-#         background_image = tk.PhotoImage(file="bg.png")
-#         background_label = tk.Label(master, image=background_image)
-#         background_label.place(x=0, y=0)
-#
-#         # Создаем холст
-#         canvas = tk.Canvas(master, width=600, height=600, bg="white")
-#         canvas.place(x=150, y=0)
-#
-#         # Создаем кнопку "Очистить"
-#         clear_button = tk.Button(master, text="Очистить", bg="#FF4136", fg="white", font=("Helvetica", 16),
-#                                       command=clear_canvas)
-#         clear_button.place(x=50, y=550, width=100, height=40)
-#
-#         # Связываем события мыши с холстом
-#         canvas.bind("<ButtonPress-1>", start_draw)
-#         canvas.bind("<B1-Motion>", draw)
-#         canvas.bind("<ButtonRelease-1>", stop_draw)
-#
-#         is_drawing = False
-#         last_x, last_y = None, None
-#
-#     def start_draw(self, event):
-#         is_drawing = True
-#         last_x, last_y = event.x, event.y
-#
-#     def draw(self, event):
-#         if is_drawing:
-#             canvas.create_line(last_x, last_y, event.x, event.y, width=3)
-#             last_x, last_y = event.x, event.y
-#
-#     def stop_draw(self, event):
-#         is_drawing = False
-#
-#     def clear_canvas(self):
-#         canvas.delete("all")
-#
-#
-# if __name__ == "__main__":
-#     root = tk.Tk()
-#     app = DrawingApp(root)
-#     root.mainloop()
-import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageDraw
+from scipy.ndimage import center_of_mass
+import math
+import cv2
+import numpy as np
+from keras.models import load_model
+
+# Загрузка обученной модели нейронной сети
+model = load_model('my_model.keras')
 
 
-# Получаем координаты курсора мыши и реализуем рисование
-def paint(event):
-    x1, y1 = (event.x - 1), (event.y - 1)
-    x2, y2 = (event.x + 1), (event.y + 1)
-    canvas.create_oval(x1, y1, x2, y2, fill="black", width=10)
-    draw.line([x1, y1, x2, y2], fill='black', width=10)
+def getBestShift(img):
+    cy, cx = center_of_mass(img)
+
+    rows, cols = img.shape
+    shiftx = np.round(cols / 2.0 - cx).astype(int)
+    shifty = np.round(rows / 2.0 - cy).astype(int)
+
+    return shiftx, shifty
 
 
-# Сохранение нарисованного в png
-def save_image():
-    FILENAME = 'image.png'
-    out = img.resize(RESIZE)  # Преобразуем изображение в 28x28 пикселей
-    out.save(FILENAME)
+def shift(img, sx, sy):
+    rows, cols = img.shape
+    M = np.float32([[1, 0, sx], [0, 1, sy]])
+    shifted = cv2.warpAffine(img, M, (cols, rows))
+    return shifted
 
 
-# Очистка холста
-def clear():
-    canvas.delete(tk.ALL)
+def recognize():
+    img = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
+    gray = 255 - img
+    # применяем пороговую обработку
+    (thresh, gray) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    # удаляем нулевые строки и столбцы
+    while np.sum(gray[0]) == 0:
+        gray = gray[1:]
+    while np.sum(gray[:, 0]) == 0:
+        gray = np.delete(gray, 0, 1)
+    while np.sum(gray[-1]) == 0:
+        gray = gray[:-1]
+    while np.sum(gray[:, -1]) == 0:
+        gray = np.delete(gray, -1, 1)
+    rows, cols = gray.shape
+
+    # изменяем размер, чтобы помещалось в box 20x20 пикселей
+    if rows > cols:
+        factor = 20.0 / rows
+        rows = 20
+        cols = int(round(cols * factor))
+        gray = cv2.resize(gray, (cols, rows))
+    else:
+        factor = 20.0 / cols
+        cols = 20
+        rows = int(round(rows * factor))
+        gray = cv2.resize(gray, (cols, rows))
+
+    # расширяем до размера 28x28
+    colsPadding = (int(math.ceil((28 - cols) / 2.0)), int(math.floor((28 - cols) / 2.0)))
+    rowsPadding = (int(math.ceil((28 - rows) / 2.0)), int(math.floor((28 - rows) / 2.0)))
+    gray = np.lib.pad(gray, (rowsPadding, colsPadding), 'constant')
+
+    # сдвигаем центр масс
+    shiftx, shifty = getBestShift(gray)
+    shifted = shift(gray, shiftx, shifty)
+    gray = shifted
+
+    cv2.imwrite('gray.jpg', gray)
+    img = gray / 255.0
+    img = np.array(img).reshape(-1, 28, 28, 1)
+    prediction = model.predict(img)
+    percent = prediction[0][np.argmax(prediction)]
+    percent = int(percent * 100)
+    digit = np.argmax(prediction)
+    print("Predicted digit:", digit)
+    print(percent)
+    return digit, f'{percent}%'
 
 
-# Создаем окно
-root = tk.Tk()
-
-# Размер окна приложения
-WIDTH = 900
-HEIGHT = 600
-
-root.geometry(f"{WIDTH}x{HEIGHT}+0+0")  # установка размера окна приложения с координатами (0,0) в левом верхнем углу
-root.resizable(False, False)
-root.title("Number recognizer")
-
-# Установка заднего фона
-# background = tk.PhotoImage(file="bg1.png")
-# background_label = tk.Label(root, image=background)
-# background_label.place(x=0, y=0, relwidth=1, relheight=1)
-root.config(bg="#AABAF2") # Установка цвета заднего фона
-
-CANVAS_WIDTH = 300  # Ширина полотна
-CANVAS_HEIGHT = 300
-WHITE = (255, 255, 255)
-RESIZE = (28, 28)
-
-# Создаем рамку
-canvas_frame = tk.Frame(root, bd=2, relief=tk.SOLID)
-canvas_frame.place(x=50, y=50, width=400, height=500)
-
-# Создаем холст внутри рамки
-canvas = tk.Canvas(canvas_frame, bg="white")
-canvas.pack(fill=tk.BOTH, expand=True)
-
-# canvas = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="white")
-
-# Привязываем обработчики событий мыши к холсту
-canvas.bind("<B1-Motion>", paint)
-
-# Копия холста на которую сохраняется нарисованная цифра
-img = Image.new('RGB', (CANVAS_WIDTH, CANVAS_HEIGHT), WHITE)
-draw = ImageDraw.Draw(img)
-
-# Создаем, отображаем и настраиваем положение кнопок
-button_frame = tk.Frame(root, bd=2, relief=tk.SOLID)
-button_frame.place(x=500, y=50, width=400, height=500)
-clear_button = tk.Button(button_frame, text="Очистить", command=clear, bg="#FF4136", fg="black", font=("Helvetica", 16))
-guess_button = tk.Button(button_frame, text="Распознать", command=save_image, bg="#44944A", fg="black", font=("Helvetica", 16))
-
-clear_button.place(x=CANVAS_WIDTH / 2, y=HEIGHT - 100)
-guess_button.place(x=CANVAS_WIDTH * 2, y=HEIGHT - 100)
-
-text = ttk.Label(text='Нарисуйте цифру', font=("Helvetica", 16))
-text.place(x=CANVAS_WIDTH // 2 - 40, y=CANVAS_HEIGHT // 2 - 50)
-
-# Запускаем главный цикл обработки событий
-root.mainloop()
+print(recognize())
